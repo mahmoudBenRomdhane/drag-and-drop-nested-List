baf176372a3aa0e2ba19f37114fc04bf5b2f2d2a
Mahmoud ben Romdhane <mahmoudbenromdhane407@gmail.com>
2022-10-31 00:28:35 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,226 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import List from "./data";
 
 function App() {
+  const list = List.getList();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <DragDropContext
+        onDragEnd={(param) => {
+          const srcI = param.source.index;
+          const desI = param.destination?.index;
+          if (desI !== null) {
+            list.splice(desI, 0, list.splice(srcI, 1)[0]);
+            List.saveList(list);
+          }
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            marginTop: "200px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="cards">
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <div className="card">
+                <div className="title-wrapper">
+                  <div className="hamburger">
+                    <div className="ham"></div>
+                    <div className="ham"></div>
+                    <div className="ham"></div>
+                  </div>
+                  <div className="title-content">Step</div>
+                </div>
+
+                <Droppable droppableId="droppable-1">
+                  {(provided, _) => (
+                    <div ref={provided.innerRef} {...provided.droppableProps}>
+                      <div className="mini-cards">
+                        {list.map((item, i) => (
+                          <Draggable
+                            key={item.id}
+                            draggableId={"draggable-" + item.id}
+                            index={i}
+                          >
+                            {(provided, snapshot) => (
+                              <div
+                                className="mini-card"
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                style={{
+                                  ...provided.draggableProps.style,
+                                  boxShadow: snapshot.isDragging
+                                    ? "0 0 .4rem #666"
+                                    : "none",
+                                }}
+                              >
+                                <div className="mini-title-wrapper">
+                                  <div
+                                    className="hamburger"
+                                    {...provided.dragHandleProps}
+                                  >
+                                    <div className="ham"></div>
+                                    <div className="ham"></div>
+                                    <div className="ham"></div>
+                                  </div>
+                                  <div className="mini-title-content">
+                                    {item.title}
+                                  </div>
+                                </div>
+                              </div>
+                            )}
+                          </Draggable>
+                        ))}
+                      </div>
+                    </div>
+                  )}
+                </Droppable>
+              </div>
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <div className="line"></div>
+                <i class="arrow down"></i>
+              </div>
+              <div className="card">
+                <div className="title-wrapper">
+                  <div className="hamburger">
+                    <div className="ham"></div>
+                    <div className="ham"></div>
+                    <div className="ham"></div>
+                  </div>
+                  <div className="title-content">Step</div>
+                </div>
+
+                <Droppable droppableId="droppable-1">
+                  {(provided, _) => (
+                    <div ref={provided.innerRef} {...provided.droppableProps}>
+                      <div className="mini-cards">
+                        {list.map((item, i) => (
+                          <Draggable
+                            key={item.id}
+                            draggableId={"draggable-" + item.id}
+                            index={i}
+                          >
+                            {(provided, snapshot) => (
+                              <div
+                                className="mini-card"
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                style={{
+                                  ...provided.draggableProps.style,
+                                  boxShadow: snapshot.isDragging
+                                    ? "0 0 .4rem #666"
+                                    : "none",
+                                }}
+                              >
+                                <div className="mini-title-wrapper">
+                                  <div
+                                    className="hamburger"
+                                    {...provided.dragHandleProps}
+                                  >
+                                    <div className="ham"></div>
+                                    <div className="ham"></div>
+                                    <div className="ham"></div>
+                                  </div>
+                                  <div className="mini-title-content">
+                                    {item.title}
+                                  </div>
+                                </div>
+                              </div>
+                            )}
+                          </Draggable>
+                        ))}
+                      </div>
+                    </div>
+                  )}
+                </Droppable>
+              </div>
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <div className="line"></div>
+                <i class="arrow down"></i>
+              </div>
+              <div className="card">
+                <div className="title-wrapper">
+                  <div className="hamburger">
+                    <div className="ham"></div>
+                    <div className="ham"></div>
+                    <div className="ham"></div>
+                  </div>
+                  <div className="title-content">Step</div>
+                </div>
+
+                <Droppable droppableId="droppable-1">
+                  {(provided, _) => (
+                    <div ref={provided.innerRef} {...provided.droppableProps}>
+                      <div className="mini-cards">
+                        {list.map((item, i) => (
+                          <Draggable
+                            key={item.id}
+                            draggableId={"draggable-" + item.id}
+                            index={i}
+                          >
+                            {(provided, snapshot) => (
+                              <div
+                                className="mini-card"
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                style={{
+                                  ...provided.draggableProps.style,
+                                  boxShadow: snapshot.isDragging
+                                    ? "0 0 .4rem #666"
+                                    : "none",
+                                }}
+                              >
+                                <div className="mini-title-wrapper">
+                                  <div
+                                    className="hamburger"
+                                    {...provided.dragHandleProps}
+                                  >
+                                    <div className="ham"></div>
+                                    <div className="ham"></div>
+                                    <div className="ham"></div>
+                                  </div>
+                                  <div className="mini-title-content">
+                                    {item.title}
+                                  </div>
+                                </div>
+                              </div>
+                            )}
+                          </Draggable>
+                        ))}
+                      </div>
+                    </div>
+                  )}
+                </Droppable>
+              </div>
+            </div>
+          </div>
+        </div>
+      </DragDropContext>
+    </>
   );
 }
 
 export default App;
+
+/*
+                <div className="mini-card">
+                  <div className="mini-title-wrapper">
+                    <div className="hamburger">
+                      <div className="ham"></div>
+                      <div className="ham"></div>
+                      <div className="ham"></div>
+                    </div>
+                    <div className="mini-title-content">Child 1</div>
+                  </div>
+                </div>
+*/
